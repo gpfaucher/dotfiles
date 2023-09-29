486e9f4026aa12dbe13e0ec632869304b9168b83
Gabriel Faucher <gpfaucher@st.noorderpoort.nl>
2023-09-29 12:51:51 +0200

    stash

--- linux/config/fish/config.fish
+++ linux/config/fish/config.fish
@@ -1,10 +1,12 @@
 source ~/.config/fish/themes/kanagawa.fish
 
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
+# # Start X at login
+# if status is-login
+#     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+#         exec startx -- -keeptty
+#     end
+# end
 
 starship init fish | source
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/gabriel/.ghcup/bin $PATH # ghcup-env
